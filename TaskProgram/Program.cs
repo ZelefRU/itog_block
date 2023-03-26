b4e739733a1541e1336893037be0e9d585e49fc9
zelef <crazzzy0meat@gmail.com>
2023-03-26 16:20:40 +0600

    add solution method

--- TaskProgram/Program.cs
+++ TaskProgram/Program.cs
@@ -1,8 +1,39 @@
 ﻿namespace TaskProgram;
 class Program
 {
+    // Написать программу, которая из имеющегося массива строк формирует массив из строк,
+    // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+    // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+    // лучше обойтись исключительно массивами.
+    
+    //     Примеры:
+    // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+    // ["1234", "1567", "-2", "computer science"] -> ["-2"]
+    // ["Russia", "Denmark", "Kazan"] -> []
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] array1 = new string[] {"hello", "2", "world", ":-)"};
+        string[] array2 = new string[] {"1234", "1567", "-2", "computer science"};
+        string[] array3 = new string[] {"Russia", "Denmark", "Kazan"};
     }
-}
+    
+    static string[] FilterThreeSybmolArray(string[] array)
+    {
+        int[] itemId = new int[array.Length];
+        int countItem = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i].Length <= 3)
+            {
+                itemId[countItem] = i;
+                countItem++;
+            }
+        }
+        string[] resultArray = new string[countItem];
+        for(int i = 0; i < countItem; i++)
+        {
+            resultArray[i] = array[itemId[i]];
+        }
+        return resultArray;
+    }
+} 
